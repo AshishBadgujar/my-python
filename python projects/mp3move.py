dfe743a54807516e015e686ef8cbf149038c42c3
Ashish <ashish@reapit.in>
2021-09-16 15:00:20 +0530

    folder-structure

--- python projects/mp3move.py
+++ python projects/mp3move.py
@@ -13,14 +13,7 @@ for path, _, files in os.walk('C:\\Users\\badgu\\Music'):
     # You might want to exclude some directories or limit search files found to directories:
     # exclude paths that have 'Program Files' folder and only include all paths that have 'Downloads',
     # ie derp/Downloads/a/ or Downloads/durp/ included, anything with 'Program Files' excluded
-    # if ('Program Files' not in path) and ('Music' in path):
-    #     # in case of windows system replace \ with /
-    #     filepath.extend([os.path.join(path, file).replace('\\', '/') for file in files
-    #                      if str(file).endswith('.mp3')])
 
-# print("filepath=", filepath)
-# for file in filepath:
-#     os.rename(filepath, os.path.split(filpath)[1])
 print("mt files=", myfiles)
 for file in myfiles:
     shutil.move(file, "C:\\Users\\badgu\\Music")
